ed00b0a78e3bd281d9cb21977494bced86afcef0
lzx <284768991@qq.com>
2019-09-14 21:27:59 +0800

    557

---
+++ src/main/java/com/lzx/leetcode/LeetCode557.java
@@ -0,0 +1,40 @@
+package com.lzx.leetcode;
+
+/**
+ * 557. 反转字符串中的单词 III
+ * https://leetcode-cn.com/problems/reverse-words-in-a-string-iii/
+ *
+ * @author : lzx
+ * created on 2019/9/14 16:56
+ */
+public class LeetCode557 implements LeetCodeProblem {
+
+
+    public void execute(Object... args) {
+        System.out.println(reverseWords((String) args[0]));
+    }
+
+
+    public String reverseWords(String s) {
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == ' ') {
+                continue;
+            }
+            int start = i;
+            while (i < chars.length && chars[i] != ' ') {
+                i++;
+            }
+            int end = i - 1;
+            while (start < end) {
+                chars[start] = (char) (chars[start] ^ chars[end]);
+                chars[end] = (char) (chars[start] ^ chars[end]);
+                chars[start] = (char) (chars[start] ^ chars[end]);
+                start++;
+                end--;
+            }
+        }
+        return new String(chars);
+    }
+
+}
---
+++ src/main/java/com/lzx/leetcode/LeetCodeProblem.java
@@ -0,0 +1,15 @@
+package com.lzx.leetcode;
+
+/**
+ * @author : lzx
+ * created on 2019/9/14 16:57
+ */
+public interface LeetCodeProblem {
+
+    /**
+     * 执行
+     *
+     * @param args 参数数组
+     */
+    void execute(Object... args);
+}
