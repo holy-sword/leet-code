37ebdde726f3163288d7d9c157c5b26eb115ac15
lzx <284768991@qq.com>
2019-09-29 23:37:36 +0800

    724

---
+++ src/main/java/com/lzx/leetcode/LeetCode724.java
@@ -0,0 +1,31 @@
+package com.lzx.leetcode;
+
+/**
+ * 724. 寻找数组的中心索引
+ * https://leetcode-cn.com/problems/find-pivot-index/
+ *
+ * @author : lzx
+ * created on 2019/9/29 23:35
+ */
+public class LeetCode724 implements LeetCodeProblem {
+
+
+    public void execute(Object... args) {
+        System.out.println(pivotIndex((int[]) args[0]));
+    }
+
+
+    public int pivotIndex(int[] nums) {
+        //sum[0]位标识位,sum[1]存放nums中第一位的值（nums[0]）
+        int[] sum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sum[i + 1] = nums[i] + sum[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (sum[i] == sum[nums.length] - sum[i + 1])
+                return i;
+        }
+        return -1;
+    }
+
+}
