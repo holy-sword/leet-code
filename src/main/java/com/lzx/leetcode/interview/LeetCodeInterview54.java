2ceb681b512f1e8645f33bca9608b24544efb251
lzx <284768991@qq.com>
2020-04-07 01:51:04 +0800

    更新说明

--- src/main/java/com/lzx/leetcode/interview/LeetCodeInterview54.java
+++ src/main/java/com/lzx/leetcode/interview/LeetCodeInterview54.java
@@ -6,7 +6,34 @@ import java.util.Arrays;
 
 /**
  * 面试题54. 二叉搜索树的第k大节点
- * https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof/
+ * <p>
+ * 给定一棵二叉搜索树，请找出其中第k大的节点。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: root = [3,1,4,null,2], k = 1
+ * -   3
+ * -  / \
+ * - 1   4
+ * -  \
+ * -   2
+ * 输出: 4
+ * 示例 2:
+ * <p>
+ * 输入: root = [5,3,6,2,4,null,null,1], k = 3
+ * -       5
+ * -      / \
+ * -     3   6
+ * -    / \
+ * -   2   4
+ * -  /
+ * - 1
+ * 输出: 4
+ * <p>
+ * 限制：
+ * 1 ≤ k ≤ 二叉搜索树元素个数
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof
  *
  * @author : lzx
  * @since 2020/4/5 20:36
